de924b156115e6eeed5e85972d980c8fe2682d0e
Yaw Premuh <156248632+YawPremuh@users.noreply.github.com>
2026-04-21 17:57:09 -0400

    Add motor4 control and update loop for all zones
    Added a new motor control for motor4 and updated the loop to run all four zones at full speed.

--- stirrer.ino
+++ stirrer.ino
@@ -2,30 +2,41 @@
 const int motor1 = 9;
 const int motor2 = 10;
 const int motor3 = 11;
+const int motor4 = 6;   // NEW motor
 
 void setup()
 {
   pinMode(motor1, OUTPUT);
   pinMode(motor2, OUTPUT);
   pinMode(motor3, OUTPUT);
-
-  Serial.begin(9600);
+  pinMode(motor4, OUTPUT);  // NEW
 }
 
 void loop()
 {
-  if (Serial.available() > 0)
-  {
-    int speed = Serial.parseInt(); // read speed from Python
-
-    speed = constrain(speed, 0, 255);
-
-    runZone1(speed);
-    runZone2(speed);
-    runZone3(speed);
-  }
+  runZone1(255); // speed: 0–255
+  runZone2(255);
+  runZone3(255);
+  runZone4(255); // NEW
 }
 
-void runZone1(int speed) { analogWrite(motor1, speed); }
-void runZone2(int speed) { analogWrite(motor2, speed); }
-void runZone3(int speed) { analogWrite(motor3, speed); }
+// Functions
+void runZone1(int speed)
+{
+  analogWrite(motor1, speed);
+}
+
+void runZone2(int speed)
+{
+  analogWrite(motor2, speed);
+}
+
+void runZone3(int speed)
+{
+  analogWrite(motor3, speed);
+}
+
+void runZone4(int speed)   // NEW
+{
+  analogWrite(motor4, speed);
+}
